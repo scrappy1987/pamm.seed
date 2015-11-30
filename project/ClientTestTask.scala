c8dab886b7e719a572e5fcd5861dd513debacdf5
Scott Thomson <scotthmsn@gmail.com>
2015-11-30 11:41:12 +0000

    Returning error when Karma unit tests fail in the build

--- project/ClientTestTask.scala
+++ project/ClientTestTask.scala
@@ -4,16 +4,20 @@ object ClientTestTask extends Build {
 
   lazy val clientTest = taskKey[Int]("clientTest")
 
-  def clientTestTask = {
+  def clientTestTask() = {
     "./node_modules/.bin/karma.cmd start ./svc/test/webapp/unit/karma.conf.js" !
   }
 
   def customTestTask = Def.taskDyn {
     val exitCode = (clientTest in Test).value
-    if (exitCode == 0)
+    if (exitCode == 0) {
       Def.task {
         (Keys.test in Test).value
       }
-    else Def.task()
+    }
+    else {
+      error("Karma exited with invalid exit code " + exitCode)
+      //throw new Exception("Karma exited with invalid exit code " + exitCode)
+    }
   }
 }
